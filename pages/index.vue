99829c255343ffb5ddab5ae1c20bc6a7c26b8ea9
dpsv74 <53926653+dpsv74@users.noreply.github.com>
2022-12-05 23:00:25 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <div class="intro">
       <h1>Nuxt Toolbox</h1>
       <p>
+         <iframe src="https://calendar.google.com/calendar/embed?height=480&wkst=1&bgcolor=%23039BE5&ctz=America%2FNew_York&mode=MONTH&src=ODdjOWE0ODY3MDcyNWUzYjdlM2U0ZTE3NzRhNDMwODgxMmRmODQ0YjE5YmVlODA2OTc4MTU5N2FkNTZlY2ZiMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%234285F4" style="border-width:0" width="600" height="480" frameborder="0" scrolling="no"></iframe>
+      </p>
+      <p>
         Hi 👋! This template gives you a
         <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
         <a href="https://www.netlify.com/products/functions/"
