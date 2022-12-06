fce88b648b67859fc1c3ae4c7c34aae6886871ff
dpsv74 <53926653+dpsv74@users.noreply.github.com>
2022-12-05 23:13:54 -0500

    Update FeedbackForm.vue

--- components/FeedbackForm.vue
+++ components/FeedbackForm.vue
@@ -19,6 +19,16 @@
       <input id="email" type="email" name="email" required />
       <label for="feedback">What is your feedback?</label>
       <textarea id="feedback" wrap="soft" name="feedback" required></textarea>
+      
+      
+      <label for="date">Pick a date</label>
+      <input type="date" name="date" id="date">
+      <br>
+
+      <label for="time">Hour</label>
+      <input type="time" id="time" name="time">
+      <br>
+          
       <button type="submit">Submit</button>
     </form>
   </div>
