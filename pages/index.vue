d34f0a081e25ed4f540031767fbe012aa6dd542c
dpsv74 <53926653+dpsv74@users.noreply.github.com>
2022-12-05 23:11:46 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,11 @@
     <div class="intro">
       <h1>Nuxt Toolbox</h1>
       <p>
-         <iframe src="https://calendar.google.com/calendar/embed?height=480&wkst=1&bgcolor=%23039BE5&ctz=America%2FNew_York&mode=MONTH&src=ODdjOWE0ODY3MDcyNWUzYjdlM2U0ZTE3NzRhNDMwODgxMmRmODQ0YjE5YmVlODA2OTc4MTU5N2FkNTZlY2ZiMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%234285F4" style="border-width:0" width="600" height="480" frameborder="0" scrolling="no"></iframe>
-      </p>
-      <p>
-        Hi 👋! This template gives you a
-        <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
-        <a href="https://www.netlify.com/products/functions/"
-          >Netlify Functions</a
-        >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
-
-      <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
-      </p>
+         <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=America%2FNew_York&showPrint=0&showTabs=0&showTz=1&showCalendars=0&src=ODdjOWE0ODY3MDcyNWUzYjdlM2U0ZTE3NzRhNDMwODgxMmRmODQ0YjE5YmVlODA2OTc4MTU5N2FkNTZlY2ZiMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%234285F4" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+     </p>
+     
     </div>
     <FeedbackForm />
-    <JokeBlock />
   </main>
 </template>
 
